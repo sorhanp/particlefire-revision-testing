a40cbbbf73f2ce66f8439efc24810b193ed421d3
sorhanp <hannupekka.sormunen@gmail.com>
2019-09-21 06:00:43 +0300

    Changes on how tests are written

--- tests/particle.test.cpp
+++ tests/particle.test.cpp
@@ -1,4 +1,11 @@
-#define BOOST_TEST_MODULE ParticleFireTests
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include <particle.h>
 
+BOOST_AUTO_TEST_CASE( test_particle_reportings )
+{
+  int a = 13, b = 12;
+  BOOST_TEST(a == b);
+  BOOST_TEST(a < b);
+  BOOST_TEST(a - 1 < b);
+  BOOST_TEST(b > a - 1);
+}
