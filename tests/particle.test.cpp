a9c0831bc50f921d984726bb95ba8bee31123674
sorhanp <hannupekka.sormunen@gmail.com>
2019-09-22 10:23:51 +0300

    Split up the tests a bit

--- tests/particle.test.cpp
+++ tests/particle.test.cpp
@@ -3,7 +3,7 @@
 
 struct ParticleFixture {
   ParticleFixture(){
-    
+
   }
   
   ~ParticleFixture(){
@@ -15,17 +15,24 @@ struct ParticleFixture {
 
 BOOST_FIXTURE_TEST_SUITE(ParticleClassTests, ParticleFixture)
 
-  BOOST_AUTO_TEST_CASE(InitTests){
-
+  BOOST_AUTO_TEST_CASE(InitTest_zero_position){
     //Particle -constructor must set single particle on middle of the screen
     BOOST_REQUIRE_EQUAL(particleF.GetXpos(), 0);
     BOOST_REQUIRE_EQUAL(particleF.GetYpos(), 0);
+  }
 
+  BOOST_AUTO_TEST_CASE(InitTest_zero_position_after_zero_update){
+    particleF.UpdatePosition(0);
+    //If no time has passed particle should have 
+    BOOST_REQUIRE_EQUAL(particleF.GetXpos(), 0);
+    BOOST_REQUIRE_EQUAL(particleF.GetYpos(), 0);
+  }
+
+  BOOST_AUTO_TEST_CASE(InitTest_zero_position_after_update){
     particleF.UpdatePosition(1);
     //When moved, particle must _not_ be on middle of the screen
     BOOST_REQUIRE_NE(particleF.GetXpos(), 0);
     BOOST_REQUIRE_NE(particleF.GetYpos(), 0);
-
   }
 
 BOOST_AUTO_TEST_SUITE_END()
