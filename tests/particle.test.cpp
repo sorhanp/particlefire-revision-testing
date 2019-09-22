abb5f8301df54ead20ce6a3335289a5c48aa9255
sorhanp <hannupekka.sormunen@gmail.com>
2019-09-22 09:40:18 +0300

    First "real" tests of Particle -class

--- tests/particle.test.cpp
+++ tests/particle.test.cpp
@@ -1,11 +1,29 @@
 #include <boost/test/unit_test.hpp>
 #include <particle.h>
 
-BOOST_AUTO_TEST_CASE( test_particle_reportings )
-{
-  int a = 13, b = 12;
-  BOOST_TEST(a == b);
-  BOOST_TEST(a < b);
-  BOOST_TEST(a - 1 < b);
-  BOOST_TEST(b > a - 1);
-}
+struct ParticleFixture {
+  ParticleFixture(){
+  }
+
+  ParticleFixture(){
+
+  }
+  particlefire::Particle particleF;
+};
+
+BOOST_FIXTURE_TEST_SUITE(ParticleClassTests, ParticleFixture)
+
+  BOOST_AUTO_TEST_CASE(InitTests){
+
+    //Particle -constructor must set single particle on middle of the screen
+    BOOST_REQUIRE_EQUAL(particleF.GetXpos(), 0);
+    BOOST_REQUIRE_EQUAL(particleF.GetYpos(), 0);
+
+    particleF.UpdatePosition(1);
+    //When moved, particle must _not_ be on middle of the screen
+    BOOST_REQUIRE_NE(particleF.GetXpos(), 0);
+    BOOST_REQUIRE_NE(particleF.GetYpos(), 0);
+
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
--- tests/screen.test.cpp
+++ tests/screen.test.cpp
@@ -1,11 +1,3 @@
 #include <boost/test/unit_test.hpp>
 #include <screen.h>
 
-BOOST_AUTO_TEST_CASE( test_screen_reportings )
-{
-  int a = 13, b = 12;
-  BOOST_TEST(a == b);
-  BOOST_TEST(a < b);
-  BOOST_TEST(a - 1 < b);
-  BOOST_TEST(b > a - 1);
-}
