0f362658d4b84051723a0aa4bb11b944454db413
sorhanp <hannupekka.sormunen@gmail.com>
2019-09-22 10:58:57 +0300

    Tests for screen class, placeholder for swarm

--- tests/screen.test.cpp
+++ tests/screen.test.cpp
@@ -1,3 +1,26 @@
 #include <boost/test/unit_test.hpp>
 #include <screen.h>
 
+// Inherit screen to get access to protected members
+struct ScreenFixture : particlefire::Screen {
+  ScreenFixture(){
+
+  }
+  
+  ~ScreenFixture(){
+
+  }
+
+  particlefire::Screen screenF;
+};
+
+BOOST_FIXTURE_TEST_SUITE(ScreenClassTests, ScreenFixture)
+
+  BOOST_AUTO_TEST_CASE(Constructor_test){
+      //If no config is provided protected data members should be default
+      BOOST_REQUIRE_EQUAL(screenF.screen_width_, 800);
+      BOOST_REQUIRE_EQUAL(screenF.screen_height_, 600);
+      BOOST_REQUIRE_EQUAL(screenF.target_fps_, 60);
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ tests/swarm.test.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp>
+#include <screen.h>
+
+struct SwarmFixture {
+  SwarmFixture(){
+
+  }
+  
+  ~SwarmFixture(){
+
+  }
+
+  particlefire::Swarm swarmF;
+};
+
+BOOST_FIXTURE_TEST_SUITE(ScreenClassTests, SwarmFixture)
+
+  BOOST_AUTO_TEST_CASE(InitTest_zero_position){
+
+  }
+
+BOOST_AUTO_TEST_SUITE_END()
